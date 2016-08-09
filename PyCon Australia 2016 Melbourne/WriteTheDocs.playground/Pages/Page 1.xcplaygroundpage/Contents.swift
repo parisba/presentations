e8bad5c20099988823c137ed85bc777e8eb7d83d
McJones <tim@lonely.coffee>
2016-08-09 15:46:03 +1000

    Initial tweaks made to move it from write the docs to pyconau

---
+++ PyCon Australia 2016 Melbourne/WriteTheDocs.playground/Pages/Page 1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,47 @@
+import UIKit
+
+/*:
+ ## Basics Putting Birds on It with Swift
+ This playground uses [Swift](http://swift.org) and Emoji to show some basic maths.
+ - - -
+ First, we need to create the world and everything in it:
+*/
+var 🌍 = "🚙📗🏠Things"
+/*:
+ * callout(Emoji is everywhere):
+ Yep, Swift supports Emoji pretty much everywhere!
+ */
+/*:
+Next, we create some temporary working space to store the world, and the birds in:
+*/
+var 💩 = ""
+/*:
+ Then, we step through everything in the world, and add a bird to it:
+*/
+for 📦 in 🌍.characters {
+    💩 = 💩 + "🐦" + String(📦)
+}
+/*:
+ And, finally, we display the world, with birds added:
+*/
+print(💩)
+
+/*:
+ - important:
+ "Never use emoji in your code."
+ \
+ –Plato
+ */
+
+/*:
+- - -
+[Next](@next)
+*/
+
+
+
+
+
+
+
+
